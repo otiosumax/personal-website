27a6697f0669995fa9d5124c72ef384289651884
PRPDSS <prpdss@gmail.com>
2026-04-03 12:49:30 +0300

    completed hero

--- src/features/home/components/Hero.tsx
+++ src/features/home/components/Hero.tsx
@@ -3,16 +3,25 @@ import "../styles/Hero.css";
 export default function Hero() {
   return (
     <div id="hero">
-      <div className="hero-content">
+      <div className="hero-content wrapper">
         <div className="hero-text">
           <h1>
-            Web-<br />
+            Web-
+            <br />
             <span>разработчик</span>
           </h1>
           <div id="separator" />
           <p>Всегда готов к новым вызовам!</p>
         </div>
-        <img src="/src/assets/stones.jpg" alt="камушки" className="hero-image" />
+        <div style={{ position: "relative" }}>
+          <p id="location">г. Москва</p>
+          <img
+            src="/src/assets/stones.jpg"
+            alt="камушки"
+            className="hero-image"
+          />
+          <div className="background-rectangle" />
+        </div>
       </div>
     </div>
   );
